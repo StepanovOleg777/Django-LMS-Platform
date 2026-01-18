0d15cc3df93197d88d20fb8713cb1181f42c385b
Олег Степанов <stepanov.2401@mail.ru>
2026-01-18 21:17:13 +0300

    Project completion: Django LMS Platform with DRF - all tasks completed

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, permissions
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ViewSet для пользователей (CRUD)"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
